69eeb9f19c66379b0c1cac0bfaacbdad0123b060
Nico <Nico@Nico-Laptop.eduroam.hs-furtwangen.de>
2018-10-09 15:20:44 +0200

    any

--- Aufgabe 0/aufgabe0script.ts
+++ Aufgabe 0/aufgabe0script.ts
@@ -6,17 +6,17 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 Er wurde nicht kopiert und auch nicht diktiert. */
 
 namespace aufgabe0 {
-        
+
     function main() {
-        var i:string = prompt("Wie heisst du?");
-        var text : string = "Hallo ";
+        var i: string = prompt("Wie heisst du?");
+        var text: string = "Hallo ";
         text += i;
         text += ", freut mich, dass du hier bist.";
 
         document.getElementById("content").innerHTML += text;
-        console.log("Hallo ",i,", freut mich, dass du hier bist.");
-        }
-
-    
-    document.addEventListener('DOMContentLoaded',main);
+        console.log("Hallo ", i, ", freut mich, dass du hier bist.");
     }
+
+
+    document.addEventListener('DOMContentLoaded', main);
+}
