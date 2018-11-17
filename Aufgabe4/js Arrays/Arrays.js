06357ba623ff67b36a8bac5a25ce25eb6e78e369
Nico <Nico@Nico-Laptop>
2018-11-17 18:48:50 +0100

    weiß

--- Aufgabe4/js Arrays/Arrays.js
+++ Aufgabe4/js Arrays/Arrays.js
@@ -8,7 +8,7 @@ var WBKonfig;
 (function (WBKonfig) {
     WBKonfig.christmasBall = [{ name: "Rot [15Euro]", price: 15 }, { name: "Gold [22Euro]", price: 22 }, { name: "Silber [18Euro]", price: 18 }];
     WBKonfig.lametta = [{ name: "Rot [3Euro]", price: 3 }, { name: "Gold [4Euro]", price: 4 }, { name: "Silber [2.50Euro]", price: 2.5 }];
-    WBKonfig.candle = [{ name: "Rot | breit [12Euro]", price: 12 }, { name: "Rot | duenn [8Euro]", price: 8 }, { name: "Gold | breit [15Euro]", price: 15 }, { name: "Gold | duenn [10Euro]", price: 10 }, { name: "Silber | breit [20Euro]", price: 20 }, { name: "Silber | duenn [17Euro]", price: 17 }, { name: "Wei� [25Euro]", price: 25 }];
+    WBKonfig.candle = [{ name: "Rot | breit [12Euro]", price: 12 }, { name: "Rot | duenn [8Euro]", price: 8 }, { name: "Gold | breit [15Euro]", price: 15 }, { name: "Gold | duenn [10Euro]", price: 10 }, { name: "Silber | breit [20Euro]", price: 20 }, { name: "Silber | duenn [17Euro]", price: 17 }, { name: "Weiss [25Euro]", price: 25 }];
     WBKonfig.tree = [{ name: "Tanne [54.99Euro]", price: 54.99 }, { name: "Nordmannstanne [75Euro]", price: 75 }, { name: "Plastiktanne [25Euro]", price: 25 }];
     WBKonfig.holder = [{ name: "Rund [25Euro]", price: 25 }, { name: "Viereckig [30Euro]", price: 30 }, { name: "Sechseckig [27.99Euro]", price: 27.99 }];
     WBKonfig.shipment = [{ name: "DHL [2.99Euro]", price: 2.99 }, { name: "UPS [2Euro]", price: 2 }, { name: "Hermes [2.50Euro]", price: 2.5 }, { name: "FedEx [3Euro]", price: 3 }];
