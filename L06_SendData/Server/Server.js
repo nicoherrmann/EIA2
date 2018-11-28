94a9369159e1d506379d0b3c317169fc8f6364d6
Nico <Nico@Nico-Laptop.hfu-open.fu.hs-furtwangen.de>
2018-11-28 14:28:22 +0100

    A6 kommentieren

---
+++ L06_SendData/Server/Server.js
@@ -0,0 +1,24 @@
+"use strict";
+const Http = require("http"); //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
+var A6;
+(function (A6) {
+    console.log("Starting server");
+    let port = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
+    if (port == undefined)
+        port = 8100;
+    let server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
+    server.addListener("request", handleRequest); //F�gt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgef�hrt
+    server.addListener("listening", handleListen); //F�gt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgef�hrt.
+    server.listen(port); //Spezifiziert, dass der Server auf den Port 8100 h�rt
+    function handleListen() {
+        console.log("Listening"); //Gib "Listening" in der Console aus
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!"); //Gib "I hear voices!" in der Console aus
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Ver�ndert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Ver�ndert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
+        _response.write(_request.url);
+        _response.end(); //response wird beendet. Dieser Aufruf muss am immer bei einem response get�tigt werden
+    }
+})(A6 || (A6 = {}));
+//# sourceMappingURL=Server.js.map
---
+++ L06_SendData/Server/Server.ts
@@ -0,0 +1,28 @@
+import * as Http from "http"; //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
+
+namespace A6 {
+    console.log("Starting server");
+    let port: number = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
+    if (port == undefined)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
+    server.addListener("request", handleRequest); //Fügt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgeführt
+    server.addListener("listening", handleListen); //Fügt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgeführt.
+    server.listen(port); //Spezifiziert, dass der Server auf den Port 8100 hört
+
+    function handleListen(): void {
+        console.log("Listening"); //Gib "Listening" in der Console aus
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //übergibt server IncomingMessage als _request und server ServerResponse als _response
+        console.log("I hear voices!"); //Gib "I hear voices!" in der Console aus
+
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Verändert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Verändert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
+
+        _response.write(_request.url); 
+        
+        _response.end(); //response wird beendet. Dieser Aufruf muss am immer bei einem response getätigt werden
+    }
+}
