287db90c977a6f20124da1b2afd994a43e96e6ad
Nico <Nico@Nico-Laptop.fritz.box>
2018-12-04 20:53:58 +0100

    server

--- L06_SendData/Server/Server.js
+++ L06_SendData/Server/Server.js
@@ -7,6 +7,7 @@ var A6;
     let port = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
     if (port == undefined)
         port = 8100;
+    let HTMLArray = [];
     let server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
     server.addListener("request", handleRequest); //F�gt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgef�hrt
     server.addListener("listening", handleListen); //F�gt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgef�hrt.
@@ -17,11 +18,28 @@ var A6;
     function handleRequest(_request, _response) {
         console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
         console.log("request");
-        let url = Url.parse(_request.url).search;
-        console.log("URLSearch:" + url);
+        //console.log("URLSearch:" + url);
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Ver�ndert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Ver�ndert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
-        _response.write(url);
+        if (_request.url != "/favicon.ico") {
+            let url = Url.parse(_request.url).search.substr(1);
+            let HTML = "";
+            for (let i = 0; i < url.length; i++) {
+                if (url[i] == "&") {
+                    HTMLArray.push(HTML);
+                    HTML = "";
+                }
+                else {
+                    HTML += url[i];
+                    console.log(HTML);
+                }
+            }
+            HTMLArray.push(HTML);
+            for (let i = 0; i < HTMLArray.length; i++) {
+                _response.write(HTMLArray[i]);
+            }
+            console.log(HTMLArray);
+        }
         _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response get�tigt werden
     } //Strg + C zum beenden
 })(A6 || (A6 = {}));
--- L06_SendData/Server/Server.ts
+++ L06_SendData/Server/Server.ts
@@ -6,7 +6,10 @@ namespace A6 {
     let port: number = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
     if (port == undefined)
         port = 8100;
-
+    
+    
+    let HTMLArray: string[] = [];
+    
     let server: Http.Server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
     server.addListener("request", handleRequest); //Fügt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgeführt
     server.addListener("listening", handleListen); //Fügt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgeführt.
@@ -18,12 +21,34 @@ namespace A6 {
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //übergibt server IncomingMessage als _request und server ServerResponse als _response
         console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
         console.log("request");
-        let url: string = Url.parse(_request.url).search;
-        console.log("URLSearch:" + url);
+
+
+        //console.log("URLSearch:" + url);
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Verändert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Verändert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
-
-        _response.write(url);
+        if (_request.url != "/favicon.ico") {
+            let url: string = Url.parse(_request.url).search.substr(1);
+            let HTML: string = "";
+            for (let i: number = 0; i < url.length; i++) {
+                if (url[i] == "&") {
+                    HTMLArray.push(HTML);
+                    HTML = "";
+                }
+                else {
+                    HTML += url[i];
+                    console.log(HTML);
+                }
+                
+                
+            }
+            HTMLArray.push(HTML);
+            
+            for (let i: number = 0 ; i < HTMLArray.length ; i++) {
+                _response.write(HTMLArray[i]);
+                }
+            console.log(HTMLArray);
+            //  _response.write(url);
+        }
 
         _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response getätigt werden
     } //Strg + C zum beenden
