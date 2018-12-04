e9087af8e9fd9f1ad17fa532771cdceea3efdc39
Nico <Nico@Nico-Laptop.fritz.box>
2018-12-04 20:59:02 +0100

    price

--- Aufgabe5v2/data/data.ts
+++ Aufgabe5v2/data/data.ts
@@ -24,23 +24,20 @@ namespace A5v2 {
     export let data: Item
         = {
             "christmasBalls": [
-                { name: "Rot [15Euro]", price: 15 },
-                { name: "Gold [22Euro]", price: 22 },
-                { name: "Silber [18Euro]", price: 18 }
+                { name: "Rot-15Euro", price: 15 },
+                { name: "Gold-22Euro", price: 22 },
+                { name: "Silber-18Euro", price: 18 }
             ],
             "lametta": [
-                { name: "Rot [3Euro]", price: 3 },
-                { name: "Gold [4Euro]", price: 4 },
-                { name: "Silber [2.50Euro]", price: 2.5 }
+                { name: "Rot-3Euro", price: 3 },
+                { name: "Gold-4Euro", price: 4 },
+                { name: "Silber-2.50Euro", price: 2.5 }
             ],
             "candle": [
-                { name: "Rot | breit [12Euro]", price: 12 },
-                { name: "Rot | duenn [8Euro]", price: 8 },
-                { name: "Gold | breit [15Euro]", price: 15 },
-                { name: "Gold | duenn [10Euro]", price: 10 },
-                { name: "Silber | breit [20Euro]", price: 20 },
-                { name: "Silber | duenn [17Euro]", price: 17 },
-                { name: "Weiss [25Euro]", price: 25 }
+                { name: "Rot-12Euro", price: 12 },
+                { name: "Gold-15Euro", price: 15 },
+                { name: "Silber-20Euro", price: 20 },
+                { name: "Weiss-25Euro", price: 25 }
             ],
            /* Ab hier neue Kategorien einfügen */ 
             
@@ -50,20 +47,20 @@ namespace A5v2 {
             
           /* Ab hier nur noch die einzelnen Kategorien anpassen */  
             "tree": [
-                { name: "Tanne [55Euro]", price: 55 },
-                { name: "Nordmannstanne [75Euro]", price: 75 },
-                { name: "Plastiktanne [25Euro]", price: 25 }
+                { name: "Tanne-55Euro", price: 55 },
+                { name: "Nordmannstanne-75Euro", price: 75 },
+                { name: "Plastiktanne-25Euro", price: 25 }
             ],
             "holder": [
-                { name: "Rund [25Euro]", price: 25 },
-                { name: "Viereckig [30Euro]", price: 30 },
-                { name: "Sechseckig [28Euro]", price: 28 }
+                { name: "Rund-25Euro", price: 25 },
+                { name: "Viereckig-30Euro", price: 30 },
+                { name: "Sechseckig-28Euro", price: 28 }
             ],
             "shipment": [
-                { name: "DHL [2.99Euro]", price: 2.99 },
-                { name: "UPS [2Euro]", price: 2 },
-                { name: "Hermes [2.50Euro]", price: 2.5 },
-                { name: "FedEx [3Euro]", price: 3 }
+                { name: "DHL-2.99Euro", price: 2.99 },
+                { name: "UPS-2Euro", price: 2 },
+                { name: "Hermes-2.50Euro", price: 2.5 },
+                { name: "FedEx-3Euro", price: 3 }
             ]
         };
 }
