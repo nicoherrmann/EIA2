e4c0c88f97a679bf54443f298ca2ac11ad1da5a7
Nico <Nico@Nico-Laptop>
2018-12-04 13:40:30 +0100

    parse

--- L06_SendData/Server/Server.js
+++ L06_SendData/Server/Server.js
@@ -1,11 +1,15 @@
 "use strict";
 const Http = require("http"); //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
+const Url = require("url");
 var A6;
 (function (A6) {
     console.log("Starting server");
     let port = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
     if (port == undefined)
         port = 8100;
+    function parse(urlStr) {
+        return Url.parse(urlStr).toString();
+    }
     let server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
     server.addListener("request", handleRequest); //F�gt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgef�hrt
     server.addListener("listening", handleListen); //F�gt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgef�hrt.
@@ -15,12 +19,13 @@ var A6;
     }
     function handleRequest(_request, _response) {
         console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
+        let querystring = require("querystring");
         let urlString = _request.url;
-        let convert = JSON.stringify(urlString);
+        let convert = parse(urlString);
         console.log("JSONstringify:" + convert);
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Ver�ndert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Ver�ndert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
-        _response.write(_request.url); //Zeigt alles nach dem / ("/" unklusive) auf der Website an
+        _response.write(convert); //Zeigt alles nach dem / ("/" unklusive) auf der Website an
         _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response get�tigt werden
     } //Strg + C zum beenden
 })(A6 || (A6 = {}));
--- L06_SendData/Server/Server.ts
+++ L06_SendData/Server/Server.ts
@@ -1,11 +1,17 @@
 import * as Http from "http"; //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
+import * as Url from "url";
 
 namespace A6 {
     console.log("Starting server");
     let port: number = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
     if (port == undefined)
         port = 8100;
-
+    
+    function parse ( urlStr: string): string {
+            return Url.parse(urlStr).toString();
+            }
+    
+    
     let server: Http.Server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
     server.addListener("request", handleRequest); //Fügt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgeführt
     server.addListener("listening", handleListen); //Fügt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgeführt.
@@ -16,13 +22,15 @@ namespace A6 {
     }
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //übergibt server IncomingMessage als _request und server ServerResponse als _response
         console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
+        let querystring : string = require("querystring");
         let urlString: string = _request.url;
-        let convert: string = JSON.stringify(urlString);
+        let convert: string = parse(urlString);
+        
         console.log("JSONstringify:" + convert);
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Verändert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Verändert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
 
-        _response.write(_request.url); //Zeigt alles nach dem / ("/" unklusive) auf der Website an
+        _response.write(convert); //Zeigt alles nach dem / ("/" unklusive) auf der Website an
         
         _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response getätigt werden
     } //Strg + C zum beenden
