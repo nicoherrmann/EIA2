9baddb63788c66b741c13484ca196ce80947767e
Nico Herrmann <nicoherrmann@users.noreply.github.com>
2020-06-23 09:39:34 +0200

    asfds

--- Prototyp2/script.ts
+++ Prototyp2/script.ts
@@ -1,21 +1,83 @@
 window.addEventListener("load", init);
 let crc2: CanvasRenderingContext2D;
+let ctx: CanvasRenderingContext2D;
+let eK1: CanvasRenderingContext2D;
 
 let canvas: HTMLCanvasElement;
+let canvas2: HTMLCanvasElement;
+let canvas3: HTMLCanvasElement;
+
 function init(): void {
     canvas = document.getElementsByTagName("canvas")[0];
     crc2 = canvas.getContext("2d");
-    crc2.fillStyle = "#FF0000";
-    crc2.fillRect(20, 20, 200, 200);
-    canvas = document.getElementsByTagName("canvas")[1];
-    crc2 = canvas.getContext("2d");
-    crc2.fillStyle = "#FFFF00";
-    crc2.fillRect(20, 20, 100, 100);
-    document.getElementById("selected_color").addEventListener("click", test);
+    crc2.fillStyle = "#F1970F";
+    crc2.fillRect(0, 0, 200, 200);
+    canvas2 = document.getElementsByTagName("canvas")[1];
+    ctx = canvas2.getContext("2d");
+    ctx.fillStyle = "#535353";
+    ctx.fillRect(0, 0, 100, 100);
+    //
+    canvas3 = document.getElementsByTagName("canvas")[2];
+    eK1 = canvas3.getContext("2d");
+    eK1.fillStyle = "#FF00FF";
+    eK1.fillRect(0, 0, 40, 40);
+    eK1.fillStyle = "#FFFF0F";
+    eK1.fillRect(0, 55, 40, 40);
+    eK1.fillStyle = "#0FF0FF";
+    eK1.fillRect(0, 110, 40, 40);
+    eK1.fillStyle = "#0000FF";
+    eK1.fillRect(0, 165, 40, 40);
+
+
+
+
+    document.getElementById("selected_color").addEventListener("input", FarbeAussen);
+    document.getElementById("selected_color2").addEventListener("input", FarbeInnen);
 }
 
-function test(): void {
+function FarbeAussen(): void {
     console.log("klappt");
     var x: any = document.getElementById("selected_color");
-    console.log(x);
+    console.log(x.value);
+    
+    crc2.clearRect(0, 0, 200, 200);
+    crc2.fillStyle = x.value;
+    crc2.fillRect(0, 0, 200, 200);
+
+    console.log("Farbe:" + x.value + "Invert:" + invertColor(x.value));
+}
+
+function FarbeInnen(): void {
+    console.log("klappt");
+    var x: any = document.getElementById("selected_color2");
+    console.log(x.value);
+    
+    ctx.clearRect(0, 0, 100, 100);
+    ctx.fillStyle = x.value;
+    ctx.fillRect(0, 0, 100, 100);
+}
+
+function invertColor(hex: string): string {
+    if (hex.indexOf("#") === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error("Invalid HEX color.");
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + g + b;
+}
+
+function padZero(str: any, len: any): string {
+    len = len || 2;
+    var zeros: string = new Array(len).join("0");
+    return (zeros + str).slice(-len);
 }
